77f612d29f2e08f15bfe1a3435d9f8afd12a0f72
Phillip Seitzer <phillipseitzer@calicolabs.com>
2021-06-11 11:59:12 -0700

    Created setup.py file.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'In silico lipid fragmentation generator (MS/MS spectra)'
+LONG_DESCRIPTION = 'In silico lipid fragmentation generator (MS/MS spectra), based on chemical standards and scientific literature.'
+
+# Setting up
+setup(
+    name="calicolipidlibrary",
+    version=VERSION,
+    author="Bryson Bennett",
+    author_email="bryson@calicolabs.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'calico', 'lipid', 'MS/MS', 'in silico', 'fragmentation', 'spectral library', 'spectrum', 'MS2'],
+    classifiers=[
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 2",
+    ]
+)
