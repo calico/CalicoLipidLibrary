fddfaf7ad923c190d97590e527c37d82ac685f6f
Phillip Seitzer <phillipseitzer@calicolabs.com>
2021-06-22 17:24:57 -0700

    Increase version number from 0.0.2 to 0.0.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'In silico lipid fragmentation generator (MS/MS spectra)'
 LONG_DESCRIPTION = 'In silico lipid fragmentation generator (MS/MS spectra), based on chemical standards and scientific literature.'
 
